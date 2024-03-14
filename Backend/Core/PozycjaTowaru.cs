b6472a2c6bb7377d4ec8ea9c1d183a83a7a8e6bc
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:09:58 +0100

    add class PozycjaTowaru in Core

---
+++ Backend/Core/PozycjaTowaru.cs
@@ -0,0 +1,15 @@
+﻿namespace Core;
+
+public class PozycjaTowaru
+{
+    public int PozycjaTowaruId { get; set; }
+    public int Ilosc { get; set; }
+    public decimal Cena { get; set; }
+
+    public int TowarId { get; set; }
+    public Towar Towar { get; set; }
+
+    public int DokumentPrzyjeciaId { get; set; }
+    public DokumentPrzyjecia DokumentPrzyjecia { get; set; }
+}
+
