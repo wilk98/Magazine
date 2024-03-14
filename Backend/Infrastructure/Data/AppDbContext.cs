dcc5c1df82cd796cb1a6f81baf28fa0dc09ae8b2
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:12:04 +0100

    create AppDbContext in Infrastructure

---
+++ Backend/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Magazyn> Magazyny { get; set; }
+    public DbSet<Dostawca> Dostawcy { get; set; }
+    public DbSet<Towar> Towary { get; set; }
+    public DbSet<Etykieta> Etykiety { get; set; }
+    public DbSet<DokumentPrzyjecia> DokumentyPrzyjecia { get; set; }
+    public DbSet<PozycjaTowaru> PozycjeTowaru { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
