69ddbafada2727d983e4c9810da22cf69fe53171
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:09:32 +0100

    add class DokumentPrzyjecia in Core

---
+++ Backend/Core/DokumentPrzyjecia.cs
@@ -0,0 +1,20 @@
+﻿namespace Core;
+
+public class DokumentPrzyjecia
+{
+    public int DokumentPrzyjeciaId { get; set; }
+    public DateTime DataPrzyjecia { get; set; }
+
+    public int MagazynId { get; set; }
+    public Magazyn Magazyn { get; set; }
+
+    public int DostawcaId { get; set; }
+    public Dostawca Dostawca { get; set; }
+
+    // Relacja wiele do wielu z Etykieta
+    public ICollection<Etykieta> Etykiety { get; set; }
+
+    // Relacja jeden do wielu z Towar
+    public ICollection<PozycjaTowaru> PozycjeTowaru { get; set; }
+}
+
