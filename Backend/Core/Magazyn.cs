aa4292fa262e2a556f637c9bf020077ffb8bd7b8
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:08:22 +0100

    add class Magazyn in Core

---
+++ Backend/Core/Magazyn.cs
@@ -0,0 +1,11 @@
+﻿namespace Core;
+
+public class Magazyn
+{
+    public int MagazynId { get; set; }
+    public string Nazwa { get; set; }
+    public string Symbol { get; set; }
+
+    // Relacje
+    public ICollection<DokumentPrzyjecia> DokumentyPrzyjecia { get; set; }
+}
