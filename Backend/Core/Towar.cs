8c9a9591a70e0e1326d2521b2d47282400065156
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:08:56 +0100

    add class Towar in Core

---
+++ Backend/Core/Towar.cs
@@ -0,0 +1,12 @@
+﻿namespace Core;
+
+public class Towar
+{
+    public int TowarId { get; set; }
+    public string Nazwa { get; set; }
+    public string Kod { get; set; }
+
+    // Relacje
+    public ICollection<PozycjaTowaru> PozycjeTowaru { get; set; }
+}
+
