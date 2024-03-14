494501ca934aa92d17178ba9709851973cdf0377
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:08:42 +0100

    add class Dostawca in Core

---
+++ Backend/Core/Dostawca.cs
@@ -0,0 +1,12 @@
+﻿namespace Core;
+
+public class Dostawca
+{
+    public int DostawcaId { get; set; }
+    public string NazwaFirmy { get; set; }
+    public string Adres { get; set; }
+
+    // Relacje
+    public ICollection<DokumentPrzyjecia> DokumentyPrzyjecia { get; set; }
+}
+
