7048ebfc322a16fa1fc28a00a30bb398b926c675
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:09:07 +0100

    add class Etykieta in Core

---
+++ Backend/Core/Etykieta.cs
@@ -0,0 +1,10 @@
+﻿namespace Core;
+
+public class Etykieta
+{
+    public int EtykietaId { get; set; }
+    public string Nazwa { get; set; }
+
+    // Relacje wiele do wielu z DokumentPrzyjecia
+    public ICollection<DokumentPrzyjecia> DokumentyPrzyjecia { get; set; }
+}
