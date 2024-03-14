ed05b2d17ec8a4c1b20138c6b9764dda02f6088c
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:11:22 +0100

    add connection to database in Program.cs

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,6 +1,16 @@
+using Infrastructure.Data;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
