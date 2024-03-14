66e9178175a21d84ee63e216a8fe68f6d4e00894
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:13:18 +0100

    add interface of GenericRepository in Infrastructure

---
+++ Backend/Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
