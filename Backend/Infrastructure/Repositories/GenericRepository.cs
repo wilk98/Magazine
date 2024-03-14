c797eab45f10e27dfe6c0df139c57bea097c3b96
wilk98 <maciekwilk09@gmail.com>
2024-03-14 14:13:28 +0100

    add GenericRepository in Infrastructure

---
+++ Backend/Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
