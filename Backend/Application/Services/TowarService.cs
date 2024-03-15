7fd8007d5e6bc8bebbbf79de8802f56808806354
wilk98 <maciekwilk09@gmail.com>
2024-03-15 16:01:01 +0100

    create TowarService

---
+++ Backend/Application/Services/TowarService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces.Services;
+using Core;
+using Infrastructure.Interfaces;
+
+namespace Application.Services;
+
+public class TowarService : ITowarService
+{
+    private readonly IGenericRepository<Towar> _towarRepository;
+
+    public TowarService(IGenericRepository<Towar> towarRepository)
+    {
+        _towarRepository = towarRepository;
+    }
+
+    public async Task<IEnumerable<Towar>> GetAllAsync()
+    {
+        return await _towarRepository.GetAllAsync();
+    }
+
+    public async Task<Towar> GetByIdAsync(int id)
+    {
+        return await _towarRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Towar> AddAsync(Towar towar)
+    {
+        return await _towarRepository.AddAsync(towar);
+    }
+
+    public async Task UpdateAsync(Towar towar)
+    {
+        await _towarRepository.UpdateAsync(towar);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var towar = await GetByIdAsync(id);
+        await _towarRepository.DeleteAsync(towar);
+    }
+}
