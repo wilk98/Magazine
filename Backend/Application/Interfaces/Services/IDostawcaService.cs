744d1a2367e1d9c2d6bdda312d8e5e5b94e5a2c7
wilk98 <maciekwilk09@gmail.com>
2024-03-15 15:58:48 +0100

    create interface of DostawcaService

---
+++ Backend/Application/Interfaces/Services/IDostawcaService.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface IDostawcaService
+{
+    Task<IEnumerable<Dostawca>> GetAllAsync();
+    Task<Dostawca> GetByIdAsync(int id);
+    Task<Dostawca> AddAsync(Dostawca dostawca);
+    Task UpdateAsync(Dostawca dostawca);
+    Task DeleteAsync(int id);
+}
+
