cefbd07906e4465796d2e17d481ff98f329641b5
wilk98 <maciekwilk09@gmail.com>
2024-03-15 15:59:13 +0100

    create interface of TowarService

---
+++ Backend/Application/Interfaces/Services/ITowarService.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface ITowarService
+{
+    Task<IEnumerable<Towar>> GetAllAsync();
+    Task<Towar> GetByIdAsync(int id);
+    Task<Towar> AddAsync(Towar towar);
+    Task UpdateAsync(Towar towar);
+    Task DeleteAsync(int id);
+}
