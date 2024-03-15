e8cd3143ad29b63b79ea77577a717f8d1f544aa4
wilk98 <maciekwilk09@gmail.com>
2024-03-15 15:58:32 +0100

    create interface of MagazineService

---
+++ Backend/Application/Interfaces/Services/IMagazynService.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface IMagazynService
+{
+    Task<IEnumerable<Magazyn>> GetAllAsync();
+    Task<Magazyn> GetByIdAsync(int id);
+    Task<Magazyn> AddAsync(Magazyn magazyn);
+    Task UpdateAsync(Magazyn magazyn);
+    Task DeleteAsync(int id);
+}
