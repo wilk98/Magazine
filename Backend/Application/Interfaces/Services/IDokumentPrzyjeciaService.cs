b05edb154f3472a82d024dff8c8b18ff1a7cc8c8
wilk98 <maciekwilk09@gmail.com>
2024-03-15 15:59:49 +0100

    create interface of DokumentPrzyjeciaService

---
+++ Backend/Application/Interfaces/Services/IDokumentPrzyjeciaService.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface IDokumentPrzyjeciaService
+{
+    Task<IEnumerable<DokumentPrzyjecia>> GetAllAsync();
+    Task<DokumentPrzyjecia> GetByIdAsync(int id);
+    Task<DokumentPrzyjecia> AddAsync(DokumentPrzyjecia dokumentPrzyjecia);
+    Task UpdateAsync(DokumentPrzyjecia dokumentPrzyjecia);
+    Task DeleteAsync(int id);
+}
