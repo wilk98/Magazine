0963d041d80823ce669e8b70e558511cfd4d27dd
wilk98 <maciekwilk09@gmail.com>
2024-03-15 16:00:11 +0100

    create interface of PozycjaTowaruService

---
+++ Backend/Application/Interfaces/Services/IPozycjaTowaruService.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface IPozycjaTowaruService
+{
+    Task<IEnumerable<PozycjaTowaru>> GetAllAsync();
+    Task<PozycjaTowaru> GetByIdAsync(int id);
+    Task<PozycjaTowaru> AddAsync(PozycjaTowaru pozycjaTowaru);
+    Task UpdateAsync(PozycjaTowaru pozycjaTowaru);
+    Task DeleteAsync(int id);
+}
+
