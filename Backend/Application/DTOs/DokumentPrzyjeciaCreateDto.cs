55e9985703e7008157758323d0f6f22a98f42ef9
wilk98 <maciekwilk09@gmail.com>
2024-03-15 17:55:39 +0100

    create DokumentPrzyjeciaCreateDto

---
+++ Backend/Application/DTOs/DokumentPrzyjeciaCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs;
+
+public class DokumentPrzyjeciaCreateDto
+{
+    public DateTime DataPrzyjecia { get; set; }
+
+    public int MagazynId { get; set; }
+    public int DostawcaId { get; set; }
+
+    public ICollection<int> EtykietyIds { get; set; }
+    public ICollection<PozycjaTowaruCreateDto> PozycjeTowaru { get; set; }
+}
