d7fd199baaf1c2088316a7e3eb8ca31c96c3d686
wilk98 <maciekwilk09@gmail.com>
2024-03-15 16:01:14 +0100

    create EtykietaService

---
+++ Backend/Application/Services/EtykietaService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces.Services;
+using Core;
+using Infrastructure.Interfaces;
+
+namespace Application.Services;
+
+public class EtykietaService : IEtykietaService
+{
+    private readonly IGenericRepository<Etykieta> _etykietaRepository;
+
+    public EtykietaService(IGenericRepository<Etykieta> etykietaRepository)
+    {
+        _etykietaRepository = etykietaRepository;
+    }
+
+    public async Task<IEnumerable<Etykieta>> GetAllAsync()
+    {
+        return await _etykietaRepository.GetAllAsync();
+    }
+
+    public async Task<Etykieta> GetByIdAsync(int id)
+    {
+        return await _etykietaRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Etykieta> AddAsync(Etykieta etykieta)
+    {
+        return await _etykietaRepository.AddAsync(etykieta);
+    }
+
+    public async Task UpdateAsync(Etykieta etykieta)
+    {
+        await _etykietaRepository.UpdateAsync(etykieta);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var etykieta = await GetByIdAsync(id);
+        await _etykietaRepository.DeleteAsync(etykieta);
+    }
+}
