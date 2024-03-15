dd266aea533833d6758471d2f5dc464a6bf58a5a
wilk98 <maciekwilk09@gmail.com>
2024-03-15 17:56:09 +0100

    create MappingProfile

---
+++ Backend/Application/DTOs/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Core;
+
+namespace Application.DTOs;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<MagazynCreateDto, Magazyn>();
+        CreateMap<DostawcaCreateDto, Dostawca>();
+        CreateMap<TowarCreateDto, Towar>();
+        CreateMap<EtykietaCreateDto, Etykieta>();
+        CreateMap<DokumentPrzyjeciaCreateDto, DokumentPrzyjecia>()
+            .ForMember(dest => dest.PozycjeTowaru, opt => opt.MapFrom(src => src.PozycjeTowaru))
+            .ForMember(dest => dest.Etykiety, opt => opt.Ignore());
+        CreateMap<PozycjaTowaruCreateDto, PozycjaTowaru>();
+    }
+}
