057019fb4196ad206e846b3491f95dbe04ecdb78
wilk98 <maciekwilk09@gmail.com>
2024-03-15 16:00:34 +0100

    create MagazynService

---
+++ Backend/Application/Services/MagazynService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces.Services;
+using Core;
+using Infrastructure.Interfaces;
+
+namespace Application.Services;
+
+public class MagazynService : IMagazynService
+{
+    private readonly IGenericRepository<Magazyn> _magazynRepository;
+
+    public MagazynService(IGenericRepository<Magazyn> magazynRepository)
+    {
+        _magazynRepository = magazynRepository;
+    }
+
+    public async Task<IEnumerable<Magazyn>> GetAllAsync()
+    {
+        return await _magazynRepository.GetAllAsync();
+    }
+
+    public async Task<Magazyn> GetByIdAsync(int id)
+    {
+        return await _magazynRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Magazyn> AddAsync(Magazyn magazyn)
+    {
+        return await _magazynRepository.AddAsync(magazyn);
+    }
+
+    public async Task UpdateAsync(Magazyn magazyn)
+    {
+        await _magazynRepository.UpdateAsync(magazyn);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var magazyn = await GetByIdAsync(id);
+        await _magazynRepository.DeleteAsync(magazyn);
+    }
+}
