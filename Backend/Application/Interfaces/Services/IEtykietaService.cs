4727a260b555fdea5d7b3f1f2de8d1a882acd09c
wilk98 <maciekwilk09@gmail.com>
2024-03-15 15:59:27 +0100

    create interface of EtykietaService

---
+++ Backend/Application/Interfaces/Services/IEtykietaService.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace Application.Interfaces.Services;
+
+public interface IEtykietaService
+{
+    Task<IEnumerable<Etykieta>> GetAllAsync();
+    Task<Etykieta> GetByIdAsync(int id);
+    Task<Etykieta> AddAsync(Etykieta etykieta);
+    Task UpdateAsync(Etykieta etykieta);
+    Task DeleteAsync(int id);
+}
