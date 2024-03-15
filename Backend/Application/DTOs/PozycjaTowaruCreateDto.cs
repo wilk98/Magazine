ca0c8b9f620ac8f36891b8c2b0000638511499ab
wilk98 <maciekwilk09@gmail.com>
2024-03-15 17:55:56 +0100

    create PozycjaTowaruCreateDto

---
+++ Backend/Application/DTOs/PozycjaTowaruCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs;
+
+public class PozycjaTowaruCreateDto
+{
+    public int Ilosc { get; set; }
+    public decimal Cena { get; set; }
+    public int TowarId { get; set; }
+}
