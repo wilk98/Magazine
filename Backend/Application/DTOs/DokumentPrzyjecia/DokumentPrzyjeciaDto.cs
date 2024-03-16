d668d15393353ff793c2907fd9c49028155d7b6a
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:41:52 +0100

    create DokumentPrzyjeciaDto

---
+++ Backend/Application/DTOs/DokumentPrzyjecia/DokumentPrzyjeciaDto.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Dostawca;
+using Application.DTOs.Etykieta;
+using Application.DTOs.Magazyn;
+using Application.DTOs.PozycjaTowaru;
+
+namespace Application.DTOs.DokumentPrzyjecia;
+
+public class DokumentPrzyjeciaDto
+{
+    public int DokumentPrzyjeciaId { get; set; }
+    public DateTime DataPrzyjecia { get; set; }
+    public int MagazynId { get; set; }
+    public MagazynDto Magazyn { get; set; }
+    public int DostawcaId { get; set; }
+    public DostawcaDto Dostawca { get; set; }
+    public List<PozycjaTowaruDto> PozycjeTowaru { get; set; }
+    public List<EtykietaDto> Etykiety { get; set; }
+}
