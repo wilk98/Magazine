4ffe42904485346dcf5d7c3c56e50a0593562a97
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:53:41 +0100

    create DokumentyPrzyjeciaController

---
+++ Backend/API/Controllers/DokumentyPrzyjeciaController.cs
@@ -0,0 +1,38 @@
+﻿using Application.DTOs.DokumentPrzyjecia;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/dokumentyprzyjecia")]
+[ApiController]
+public class DokumentyPrzyjeciaController : ControllerBase
+{
+    private readonly IDokumentPrzyjeciaService _dokumentPrzyjeciaService;
+
+    public DokumentyPrzyjeciaController(IDokumentPrzyjeciaService dokumentPrzyjeciaService)
+    {
+        _dokumentPrzyjeciaService = dokumentPrzyjeciaService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DokumentPrzyjeciaDto>>> GetAll()
+    {
+        var dokumentyDto = await _dokumentPrzyjeciaService.GetAllAsync();
+        return Ok(dokumentyDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<DokumentPrzyjeciaDto>> Get(int id)
+    {
+        var dokumentDto = await _dokumentPrzyjeciaService.GetByIdAsync(id);
+        if (dokumentDto == null) return NotFound();
+        return Ok(dokumentDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DokumentPrzyjeciaDto>> Create([FromBody] DokumentPrzyjeciaCreateDto dokumentCreateDto)
+    {
+        var createdDokumentDto = await _dokumentPrzyjeciaService.AddAsync(dokumentCreateDto);
+        return CreatedAtAction(nameof(Get), new { id = createdDokumentDto.DokumentPrzyjeciaId }, createdDokumentDto);
+    }
+
+}
