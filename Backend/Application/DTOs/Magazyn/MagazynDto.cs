9a1aec0000ccc26bf93288f69db9dcf2aec2b9d8
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:40:40 +0100

    create MagazynDto

---
+++ Backend/Application/DTOs/Magazyn/MagazynDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.Magazyn;
+
+public class MagazynDto
+{
+    public int MagazynId { get; set; }
+    public string Nazwa { get; set; }
+    public string Symbol { get; set; }
+}
+
