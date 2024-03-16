ff6751ed2340e82fdf583192358db03027bcfda1
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:46:35 +0100

    add AutoMapper to EtykietaService

--- Backend/Application/Services/EtykietaService.cs
+++ Backend/Application/Services/EtykietaService.cs
@@ -1,41 +1,52 @@
-﻿using Application.Interfaces.Services;
+﻿using Application.DTOs.Etykieta;
+using Application.Interfaces.Services;
+using AutoMapper;
 using Core;
 using Infrastructure.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Application.Services;
 
 public class EtykietaService : IEtykietaService
 {
     private readonly IGenericRepository<Etykieta> _etykietaRepository;
+    private readonly IMapper _mapper;
 
-    public EtykietaService(IGenericRepository<Etykieta> etykietaRepository)
+    public EtykietaService(IGenericRepository<Etykieta> etykietaRepository, IMapper mapper)
     {
         _etykietaRepository = etykietaRepository;
+        _mapper = mapper;
     }
 
-    public async Task<IEnumerable<Etykieta>> GetAllAsync()
+    public async Task<IEnumerable<EtykietaDto>> GetAllAsync()
     {
-        return await _etykietaRepository.GetAllAsync();
+        var etykiety = await _etykietaRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<EtykietaDto>>(etykiety);
     }
 
-    public async Task<Etykieta> GetByIdAsync(int id)
+    public async Task<EtykietaDto> GetByIdAsync(int id)
     {
-        return await _etykietaRepository.GetByIdAsync(id);
+        var etykieta = await _etykietaRepository.GetByIdAsync(id);
+        return _mapper.Map<EtykietaDto>(etykieta);
     }
 
-    public async Task<Etykieta> AddAsync(Etykieta etykieta)
+    public async Task<EtykietaDto> AddAsync(EtykietaCreateDto etykietaCreateDto)
     {
-        return await _etykietaRepository.AddAsync(etykieta);
+        var etykieta = _mapper.Map<Etykieta>(etykietaCreateDto);
+        etykieta = await _etykietaRepository.AddAsync(etykieta);
+        return _mapper.Map<EtykietaDto>(etykieta);
     }
 
-    public async Task UpdateAsync(Etykieta etykieta)
+    public async Task UpdateAsync(EtykietaDto etykietaDto)
     {
+        var etykieta = _mapper.Map<Etykieta>(etykietaDto);
         await _etykietaRepository.UpdateAsync(etykieta);
     }
 
     public async Task DeleteAsync(int id)
     {
-        var etykieta = await GetByIdAsync(id);
+        var etykieta = await _etykietaRepository.GetByIdAsync(id);
         await _etykietaRepository.DeleteAsync(etykieta);
     }
 }
