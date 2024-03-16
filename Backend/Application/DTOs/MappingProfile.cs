5e9a9e811e99443887dc9d1d200d80bdac8b55e1
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:42:31 +0100

    add more connections in MappingProfile

--- Backend/Application/DTOs/MappingProfile.cs
+++ Backend/Application/DTOs/MappingProfile.cs
@@ -1,19 +1,40 @@
-﻿using AutoMapper;
+﻿using Application.DTOs.DokumentPrzyjecia;
+using Application.DTOs.Dostawca;
+using Application.DTOs.Etykieta;
+using Application.DTOs.Magazyn;
+using Application.DTOs.PozycjaTowaru;
+using Application.DTOs.Towar;
+using AutoMapper;
 using Core;
 
-namespace Application.DTOs;
+namespace Application;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
         CreateMap<MagazynCreateDto, Magazyn>();
+        CreateMap<MagazynDto, Magazyn>();
+        CreateMap<Magazyn, MagazynDto>();
+
         CreateMap<DostawcaCreateDto, Dostawca>();
+        CreateMap<DostawcaDto, Dostawca>();
+        CreateMap<Dostawca, DostawcaDto>();
+
         CreateMap<TowarCreateDto, Towar>();
+        CreateMap<TowarDto, Towar>();
+        CreateMap<Towar, TowarDto>();
+
         CreateMap<EtykietaCreateDto, Etykieta>();
-        CreateMap<DokumentPrzyjeciaCreateDto, DokumentPrzyjecia>()
-            .ForMember(dest => dest.PozycjeTowaru, opt => opt.MapFrom(src => src.PozycjeTowaru))
-            .ForMember(dest => dest.Etykiety, opt => opt.Ignore());
+        CreateMap<EtykietaDto, Etykieta>();
+        CreateMap<Etykieta, EtykietaDto>();
+
+        CreateMap<DokumentPrzyjeciaCreateDto, DokumentPrzyjecia>();
+        CreateMap<DokumentPrzyjeciaDto, DokumentPrzyjecia>();
+        CreateMap<DokumentPrzyjecia, DokumentPrzyjeciaDto>();
+
         CreateMap<PozycjaTowaruCreateDto, PozycjaTowaru>();
+        CreateMap<PozycjaTowaruDto, PozycjaTowaru>();
+        CreateMap<PozycjaTowaru, PozycjaTowaruDto>();
     }
 }
