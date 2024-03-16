c2f8e5cd1f23f69e9c2d83d5b1e4e0b8b6294e4c
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:46:02 +0100

    add AutoMapper to DostawcaService

--- Backend/Application/Services/DostawcaService.cs
+++ Backend/Application/Services/DostawcaService.cs
@@ -1,41 +1,52 @@
-﻿using Application.Interfaces.Services;
+﻿using Application.DTOs.Dostawca;
+using Application.Interfaces.Services;
+using AutoMapper;
 using Core;
 using Infrastructure.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Application.Services;
 
 public class DostawcaService : IDostawcaService
 {
     private readonly IGenericRepository<Dostawca> _dostawcaRepository;
+    private readonly IMapper _mapper;
 
-    public DostawcaService(IGenericRepository<Dostawca> dostawcaRepository)
+    public DostawcaService(IGenericRepository<Dostawca> dostawcaRepository, IMapper mapper)
     {
         _dostawcaRepository = dostawcaRepository;
+        _mapper = mapper;
     }
 
-    public async Task<IEnumerable<Dostawca>> GetAllAsync()
+    public async Task<IEnumerable<DostawcaDto>> GetAllAsync()
     {
-        return await _dostawcaRepository.GetAllAsync();
+        var dostawcy = await _dostawcaRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<DostawcaDto>>(dostawcy);
     }
 
-    public async Task<Dostawca> GetByIdAsync(int id)
+    public async Task<DostawcaDto> GetByIdAsync(int id)
     {
-        return await _dostawcaRepository.GetByIdAsync(id);
+        var dostawca = await _dostawcaRepository.GetByIdAsync(id);
+        return _mapper.Map<DostawcaDto>(dostawca);
     }
 
-    public async Task<Dostawca> AddAsync(Dostawca dostawca)
+    public async Task<DostawcaDto> AddAsync(DostawcaCreateDto dostawcaCreateDto)
     {
-        return await _dostawcaRepository.AddAsync(dostawca);
+        var dostawca = _mapper.Map<Dostawca>(dostawcaCreateDto);
+        dostawca = await _dostawcaRepository.AddAsync(dostawca);
+        return _mapper.Map<DostawcaDto>(dostawca);
     }
 
-    public async Task UpdateAsync(Dostawca dostawca)
+    public async Task UpdateAsync(DostawcaDto dostawcaDto)
     {
+        var dostawca = _mapper.Map<Dostawca>(dostawcaDto);
         await _dostawcaRepository.UpdateAsync(dostawca);
     }
 
     public async Task DeleteAsync(int id)
     {
-        var dostawca = await GetByIdAsync(id);
+        var dostawca = await _dostawcaRepository.GetByIdAsync(id);
         await _dostawcaRepository.DeleteAsync(dostawca);
     }
 }
