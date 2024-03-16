f43ac0799cf5886727012d84156f6640bfe60d34
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:46:20 +0100

    add AutoMapper to TowarService

--- Backend/Application/Services/TowarService.cs
+++ Backend/Application/Services/TowarService.cs
@@ -1,41 +1,52 @@
-﻿using Application.Interfaces.Services;
+﻿using Application.DTOs.Towar;
+using Application.Interfaces.Services;
+using AutoMapper;
 using Core;
 using Infrastructure.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Application.Services;
 
 public class TowarService : ITowarService
 {
     private readonly IGenericRepository<Towar> _towarRepository;
+    private readonly IMapper _mapper;
 
-    public TowarService(IGenericRepository<Towar> towarRepository)
+    public TowarService(IGenericRepository<Towar> towarRepository, IMapper mapper)
     {
         _towarRepository = towarRepository;
+        _mapper = mapper;
     }
 
-    public async Task<IEnumerable<Towar>> GetAllAsync()
+    public async Task<IEnumerable<TowarDto>> GetAllAsync()
     {
-        return await _towarRepository.GetAllAsync();
+        var towary = await _towarRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<TowarDto>>(towary);
     }
 
-    public async Task<Towar> GetByIdAsync(int id)
+    public async Task<TowarDto> GetByIdAsync(int id)
     {
-        return await _towarRepository.GetByIdAsync(id);
+        var towar = await _towarRepository.GetByIdAsync(id);
+        return _mapper.Map<TowarDto>(towar);
     }
 
-    public async Task<Towar> AddAsync(Towar towar)
+    public async Task<TowarDto> AddAsync(TowarCreateDto towarCreateDto)
     {
-        return await _towarRepository.AddAsync(towar);
+        var towar = _mapper.Map<Towar>(towarCreateDto);
+        towar = await _towarRepository.AddAsync(towar);
+        return _mapper.Map<TowarDto>(towar);
     }
 
-    public async Task UpdateAsync(Towar towar)
+    public async Task UpdateAsync(TowarDto towarDto)
     {
+        var towar = _mapper.Map<Towar>(towarDto);
         await _towarRepository.UpdateAsync(towar);
     }
 
     public async Task DeleteAsync(int id)
     {
-        var towar = await GetByIdAsync(id);
+        var towar = await _towarRepository.GetByIdAsync(id);
         await _towarRepository.DeleteAsync(towar);
     }
 }
