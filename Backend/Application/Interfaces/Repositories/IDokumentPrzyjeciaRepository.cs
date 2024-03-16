3cfcd2ce4fc02d163a0ee16ff321870e30c320f4
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:49:28 +0100

    create interface of DokumentPrzyjeciaRepository

---
+++ Backend/Application/Interfaces/Repositories/IDokumentPrzyjeciaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+using Infrastructure.Interfaces;
+
+namespace Application.Interfaces.Repositories;
+
+public interface IDokumentPrzyjeciaRepository
+{
+    Task<IEnumerable<DokumentPrzyjecia>> GetAllAsync();
+    Task<DokumentPrzyjecia> GetByIdAsync(int id);
+    Task<DokumentPrzyjecia> AddAsync(DokumentPrzyjecia dokumentPrzyjecia, List<int> etykietyIds = null);
+
+}
