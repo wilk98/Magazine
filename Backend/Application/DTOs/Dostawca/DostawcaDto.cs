a96e03e9c7bca5e71b1aa0a40a1e71f6f440b954
wilk98 <maciekwilk09@gmail.com>
2024-03-16 19:40:55 +0100

    create DostawcaDto

---
+++ Backend/Application/DTOs/Dostawca/DostawcaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Dostawca;
+
+public class DostawcaDto
+{
+    public int DostawcaId { get; set; }
+    public string NazwaFirmy { get; set; }
+    public string Adres { get; set; }
+}
