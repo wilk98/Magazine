530a6329b8a033b4f77dff93d1560805a6bcbee0
wilk98 <maciekwilk09@gmail.com>
2024-03-17 01:59:59 +0100

    create DokumentPrzyjeciaUpdateDto i PozycjaTowaruUpdateDto

---
+++ Backend/Application/DTOs/DokumentPrzyjecia/DokumentPrzyjeciaUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.PozycjaTowaru;
+
+namespace Application.DTOs.DokumentPrzyjecia;
+
+public class DokumentPrzyjeciaUpdateDto
+{
+    public int DokumentPrzyjeciaId { get; set; }
+    public DateTime DataPrzyjecia { get; set; }
+    public int MagazynId { get; set; }
+    public int DostawcaId { get; set; }
+    public List<int> EtykietyIds { get; set; }
+    public List<PozycjaTowaruUpdateDto> PozycjeTowaru { get; set; }
+}
---
+++ Backend/Application/DTOs/PozycjaTowaru/PozycjaTowaruUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.PozycjaTowaru;
+
+public class PozycjaTowaruUpdateDto
+{
+    public int PozycjaTowaruId { get; set; }
+    public int Ilosc { get; set; }
+    public decimal Cena { get; set; }
+    public int TowarId { get; set; }
+}
