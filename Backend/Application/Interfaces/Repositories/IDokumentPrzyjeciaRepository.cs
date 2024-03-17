144f269136971871bfc404428fe5c9e5957f1e61
wilk98 <maciekwilk09@gmail.com>
2024-03-17 02:00:42 +0100

    update interface of DokumentPrzyjeciaRepository

--- Backend/Application/Interfaces/Repositories/IDokumentPrzyjeciaRepository.cs
+++ Backend/Application/Interfaces/Repositories/IDokumentPrzyjeciaRepository.cs
@@ -1,4 +1,5 @@
-﻿using Core;
+﻿using Application.DTOs.DokumentPrzyjecia;
+using Core;
 using Infrastructure.Interfaces;
 
 namespace Application.Interfaces.Repositories;
@@ -8,5 +9,7 @@ public interface IDokumentPrzyjeciaRepository
     Task<IEnumerable<DokumentPrzyjecia>> GetAllAsync();
     Task<DokumentPrzyjecia> GetByIdAsync(int id);
     Task<DokumentPrzyjecia> AddAsync(DokumentPrzyjecia dokumentPrzyjecia, List<int> etykietyIds = null);
-
+    Task ZatwierdzDokumentPrzyjeciaAsync(int dokumentPrzyjeciaId);
+    Task AnulujDokumentPrzyjeciaAsync(int dokumentPrzyjeciaId);
+    Task UpdateAsync(DokumentPrzyjeciaUpdateDto dokumentUpdateDto);
 }
