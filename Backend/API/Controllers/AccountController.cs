9481b11bad60a1f9f4721896a5e561b11ad0a3d9
wilk98 <maciekwilk09@gmail.com>
2024-03-17 02:27:44 +0100

    create AccountController

---
+++ Backend/API/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs.User;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto model)
+    {
+        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            // Pomyślna rejestracja
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(result.Errors);
+        }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto model)
+    {
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+        {
+            // Pomyślne logowanie
+            return Ok();
+        }
+        else
+        {
+            return Unauthorized();
+        }
+    }
+
+}
