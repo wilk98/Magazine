e91355e81245bed5de15583c56eaec1e6ddab583
wilk98 <maciekwilk09@gmail.com>
2024-03-18 01:30:55 +0100

    add routes in app.routes

--- Frontend/magazyn-app/src/app/app.routes.ts
+++ Frontend/magazyn-app/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { MagazynyComponent } from './components/lista-magazynow/magazyny.component';
-
+import { TowaryComponent } from './components/lista-towarow/towary.component';
+import { DostawcyComponent } from './components/lista-dostawcow/dostawcy.component';
+import { EtykietyComponent } from './components/lista-etykiet/etykiety.component';
 
 export const routes: Routes = [
     { path: 'magazyny', component: MagazynyComponent },
+    { path: 'towary', component: TowaryComponent },
+    { path: 'dostawcy', component: DostawcyComponent },
+    { path: 'etykiety', component: EtykietyComponent },
     { path: '', redirectTo: '/magazyny', pathMatch: 'full' }
 ];
