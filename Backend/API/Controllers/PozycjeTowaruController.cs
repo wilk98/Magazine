9be8011ec6c4fbc0ded92b4ddd1614f8d00751a0
wilk98 <maciekwilk09@gmail.com>
2024-03-18 23:38:19 +0100

    create PozycjeTowaruController

---
+++ Backend/API/Controllers/PozycjeTowaruController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PozycjeTowaruController : ControllerBase
+{
+    private readonly IPozycjaTowaruService _pozycjeTowaruService;
+
+    public PozycjeTowaruController(IPozycjaTowaruService pozycjeTowaruService)
+    {
+        _pozycjeTowaruService = pozycjeTowaruService;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePozycjaTowaru(int id)
+    {
+        await _pozycjeTowaruService.DeleteAsync(id);
+        return Ok();
+
+    }
+}
