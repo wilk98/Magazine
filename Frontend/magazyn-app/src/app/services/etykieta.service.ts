6d1334ed40b4e6b4b32cb12d184a97c5b18b5b41
wilk98 <maciekwilk09@gmail.com>
2024-03-18 03:06:32 +0100

    create Add, Edit and Delete in etykieta.service

--- Frontend/magazyn-app/src/app/services/etykieta.service.ts
+++ Frontend/magazyn-app/src/app/services/etykieta.service.ts
@@ -15,4 +15,16 @@ export class EtykietaService {
   getEtykiety(): Observable<Etykieta[]> {
     return this.http.get<Etykieta[]>(this.apiUrl);
   }
+
+  addEtykieta(etykieta: Etykieta): Observable<Etykieta> {
+    return this.http.post<Etykieta>(this.apiUrl, etykieta);
+  }
+
+  editEtykieta(etykieta: Etykieta): Observable<Etykieta> {
+    return this.http.put<Etykieta>(`${this.apiUrl}/${etykieta.etykietaId}`, etykieta);
+  }
+
+  deleteEtykieta(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
