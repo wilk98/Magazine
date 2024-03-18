f11b5953139fca020c66b36f6941b4b418ab674e
wilk98 <maciekwilk09@gmail.com>
2024-03-18 23:42:12 +0100

    update app.routes

--- Frontend/magazyn-app/src/app/app.routes.ts
+++ Frontend/magazyn-app/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
-import { MagazynyComponent } from './components/lista-magazynow/magazyny.component';
-import { TowaryComponent } from './components/lista-towarow/towary.component';
-import { DostawcyComponent } from './components/lista-dostawcow/dostawcy.component';
-import { EtykietyComponent } from './components/lista-etykiet/etykiety.component';
+import { MagazynyComponent } from './components/magazyny/magazyny.component';
+import { TowaryComponent } from './components/towary/towary.component';
+import { DostawcyComponent } from './components/dostawcy/dostawcy.component';
+import { EtykietyComponent } from './components/etykiety/etykiety.component';
+import { DokumentyComponent } from './components/dokumenty/dokumenty.component';
+
 
 export const routes: Routes = [
     { path: 'magazyny', component: MagazynyComponent },
     { path: 'towary', component: TowaryComponent },
     { path: 'dostawcy', component: DostawcyComponent },
     { path: 'etykiety', component: EtykietyComponent },
+    { path: 'dokumenty', component: DokumentyComponent },
     { path: '', redirectTo: '/magazyny', pathMatch: 'full' }
 ];
