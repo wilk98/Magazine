6e5290b1e52596ee588984942a653b298860e5d6
wilk98 <maciekwilk09@gmail.com>
2024-03-18 03:06:49 +0100

    create Add, Edit and Delete in etykiety.component

--- Frontend/magazyn-app/src/app/components/lista-etykiet/etykiety.component.ts
+++ Frontend/magazyn-app/src/app/components/lista-etykiet/etykiety.component.ts
@@ -8,6 +8,8 @@ import { EtykietaService } from '../../services/etykieta.service';
 })
 export class EtykietyComponent implements OnInit {
   etykiety: Etykieta[] = [];
+  showAddEditForm = false;
+  currentEditEtykieta: Etykieta = { etykietaId: 0, nazwa: '' };
 
   constructor(private etykietaService: EtykietaService) { }
 
@@ -18,6 +20,35 @@ export class EtykietyComponent implements OnInit {
   loadEtykiety(): void {
     this.etykietaService.getEtykiety().subscribe((data: Etykieta[]) => {
       this.etykiety = data;
+    });
+  }
+
+  showAddEtykieta(): void {
+    this.currentEditEtykieta = { etykietaId: 0, nazwa: '' };
+    this.showAddEditForm = true;
+  }
+
+  openEditEtykieta(etykieta: Etykieta): void {
+    this.currentEditEtykieta = { ...etykieta };
+    this.showAddEditForm = true;
+  }
+
+  saveEtykieta(): void {
+    if (this.currentEditEtykieta.etykietaId) {
+      this.etykietaService.editEtykieta(this.currentEditEtykieta).subscribe(() => {
+        this.loadEtykiety();
+      });
+    } else {
+      this.etykietaService.addEtykieta(this.currentEditEtykieta).subscribe(() => {
+        this.loadEtykiety();
+      });
+    }
+    this.showAddEditForm = false;
+  }
+
+  deleteEtykieta(id: number): void {
+    this.etykietaService.deleteEtykieta(id).subscribe(() => {
+      this.loadEtykiety();
     });
   }
 }
