fe6c1e44f1228b4e1ccb19802261c0d676fdaffc
wilk98 <maciekwilk09@gmail.com>
2024-03-18 02:29:15 +0100

    update magazyn.component (Add, Update, Delete)

--- Frontend/magazyn-app/src/app/components/lista-magazynow/magazyny.component.ts
+++ Frontend/magazyn-app/src/app/components/lista-magazynow/magazyny.component.ts
@@ -4,10 +4,12 @@ import { Magazyn } from '../../models/magazyn.model';
 
 @Component({
   selector: 'app-magazyny',
-  templateUrl: 'magazyny.component.html',
+  templateUrl: './magazyny.component.html',
 })
 export class MagazynyComponent implements OnInit {
   magazyny: Magazyn[] = [];
+  showAddEditForm = false;
+  currentEditMagazyn: Magazyn = { magazynId: 0, nazwa: '', symbol: '' };
 
   constructor(private magazynService: MagazynService) { }
 
@@ -18,6 +20,36 @@ export class MagazynyComponent implements OnInit {
   loadMagazyny(): void {
     this.magazynService.getMagazyny().subscribe((data: Magazyn[]) => {
       this.magazyny = data;
+    });
+  }
+
+  showAddMagazyn(): void {
+    this.currentEditMagazyn = { magazynId: 0, nazwa: '', symbol: '' };
+    this.showAddEditForm = true;
+  }
+
+  openEditMagazyn(magazyn: Magazyn): void {
+    this.currentEditMagazyn = { ...magazyn };
+    this.showAddEditForm = true;
+  }
+
+  saveMagazyn(): void {
+    if (this.currentEditMagazyn.magazynId) {
+      this.magazynService.editMagazyn(this.currentEditMagazyn).subscribe(() => {
+        this.loadMagazyny();
+      });
+    } else {
+      this.magazynService.addMagazyn(this.currentEditMagazyn).subscribe(() => {
+        this.loadMagazyny();
+      });
+    }
+    this.showAddEditForm = false;
+  }
+
+  deleteMagazyn(id: number): void {
+    console
+    this.magazynService.deleteMagazyn(id).subscribe(() => {
+      this.loadMagazyny();
     });
   }
 }
