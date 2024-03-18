c3fb62b1f81be7f79ee92ed55d33be6ce7e0de1b
wilk98 <maciekwilk09@gmail.com>
2024-03-18 01:31:11 +0100

    update app.module

--- Frontend/magazyn-app/src/app/app.module.ts
+++ Frontend/magazyn-app/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MagazynyComponent } from './components/lista-magazynow/magazyny.component';
+import { TowaryComponent } from './components/lista-towarow/towary.component';
+import { DostawcyComponent } from './components/lista-dostawcow/dostawcy.component';
+import { EtykietyComponent } from './components/lista-etykiet/etykiety.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MagazynyComponent
+    MagazynyComponent,
+    TowaryComponent,
+    DostawcyComponent,
+    EtykietyComponent
   ],
   imports: [
     BrowserModule,
