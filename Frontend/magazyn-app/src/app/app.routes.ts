3e9a5f1032451656eb9ccec0d1491ad1c82e5ebe
wilk98 <maciekwilk09@gmail.com>
2024-03-18 01:15:19 +0100

    add routes in app.routes

--- Frontend/magazyn-app/src/app/app.routes.ts
+++ Frontend/magazyn-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MagazynyComponent } from './components/lista-magazynow/magazyny.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'magazyny', component: MagazynyComponent },
+    { path: '', redirectTo: '/magazyny', pathMatch: 'full' }
+];
