b3046ee40a57aacb1d18c86fc33f492eaee66fc7
wilk98 <maciekwilk09@gmail.com>
2024-03-18 02:28:56 +0100

    update magazyn.service (Add, Update, Delete)

--- Frontend/magazyn-app/src/app/services/magazyn.service.ts
+++ Frontend/magazyn-app/src/app/services/magazyn.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Magazyn } from '../models/magazyn.model';
 import { AppConfig } from '../config/app.config';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,5 +15,17 @@ export class MagazynService {
 
   getMagazyny(): Observable<Magazyn[]> {
     return this.http.get<Magazyn[]>(this.apiUrl);
+  }
+
+  addMagazyn(magazyn: Magazyn): Observable<Magazyn> {
+    return this.http.post<Magazyn>(this.apiUrl, magazyn);
+  }
+
+  editMagazyn(magazyn: Magazyn): Observable<Magazyn> {
+    return this.http.put<Magazyn>(`${this.apiUrl}/${magazyn.magazynId}`, magazyn);
+  }
+
+  deleteMagazyn(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
