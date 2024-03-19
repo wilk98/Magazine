7f57ad10e2ec7c890b1774dfc9f5f99f55b980a0
wilk98 <maciekwilk09@gmail.com>
2024-03-19 01:54:03 +0100

    update app.routes

--- Frontend/magazyn-app/src/app/app.routes.ts
+++ Frontend/magazyn-app/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { TowaryComponent } from './components/towary/towary.component';
 import { DostawcyComponent } from './components/dostawcy/dostawcy.component';
 import { EtykietyComponent } from './components/etykiety/etykiety.component';
 import { DokumentyComponent } from './components/dokumenty/dokumenty.component';
+import { LoginComponent } from './components/logowanie/login.component';
+import { RegisterComponent } from './components/rejestracja/register.component';
+
 
 
 export const routes: Routes = [
@@ -11,6 +14,9 @@ export const routes: Routes = [
     { path: 'towary', component: TowaryComponent },
     { path: 'dostawcy', component: DostawcyComponent },
     { path: 'etykiety', component: EtykietyComponent },
+    { path: 'dokumenty', component: DokumentyComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'dokumenty', component: DokumentyComponent },
     { path: '', redirectTo: '/magazyny', pathMatch: 'full' }
 ];
