cb47bfc4e0480b1c3529919adfafa5d186163729
wilk98 <maciekwilk09@gmail.com>
2024-03-19 01:53:51 +0100

    add auth.service

---
+++ Frontend/magazyn-app/src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppConfig } from '../config/app.config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')!) : null);
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+  
+
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${AppConfig.apiUrl}account/login`, { email, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
