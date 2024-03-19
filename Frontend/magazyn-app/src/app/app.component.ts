d50cf52224aa47c839969ab48208bc4dce9c6c42
wilk98 <maciekwilk09@gmail.com>
2024-03-19 01:53:36 +0100

    update app.component

--- Frontend/magazyn-app/src/app/app.component.ts
+++ Frontend/magazyn-app/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isUserLoggedIn: boolean = false;
+  currentUserEmail?: string;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.currentUser.subscribe(user => {
+      this.isUserLoggedIn = !!user;
+      this.currentUserEmail = user?.email || undefined; 
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
